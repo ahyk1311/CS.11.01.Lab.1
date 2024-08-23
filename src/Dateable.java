dea834b3bc021678cff602c3ed5e9cfce0457d74
ahyk1311 <alyshahyk2027@student.cis.edu.hk>
2024-08-23 12:59:03 +0800

    .

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your age:");
+        int age = scanner.nextInt() /2 +7;
+        System.out.println("Your dateable age is " + age + ".");
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your age:");
+        int age = 100 - scanner.nextInt();
+        System.out.println("Try reading " + age + " pages before giving up on your book.");
     }
 }
